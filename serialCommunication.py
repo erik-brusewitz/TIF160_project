b5fefee779fba9382d5e0fdbe83ca269060664c7
lucabeber <luca.beber@studenti.unitn.it>
2022-09-19 11:39:11 +0200

    python communication

---
+++ serialCommunication.py
@@ -0,0 +1,26 @@
+import serial
+import time
+
+arduino = serial.Serial(port='/dev/ttyACM0', baudrate=57600, timeout=.1)
+
+def check_for_return_value(value):
+    for i in range(10):
+        data = str(arduino.readline())
+        data = data.replace("b", "")
+        data = data.replace("'", "")
+        print("value = " + str(value) + ", data = " + str(data))
+        if data == value:
+            return True
+        time.sleep(0.2)
+    
+    return False
+
+def send_package(data):
+    arduino.write(bytes(data, 'utf-8'))
+    if check_for_return_value(data):
+        print("Data sent anreceivesuccessfully")
+        return 0
+    else:
+        print("Data sending failed")
+        return 1
+
