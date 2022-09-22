e6a1325eaeb38967641a4b20e976a229fc15656d
Aishwaryak2998 <aishwarya2998@gmail.com>
2022-09-22 09:33:23 +0200

    Shape detection python file

---
+++ Shape_detection.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2, math
+cap = cv2.VideoCapture(1,cv2.CAP_DSHOW) #cv2.CAP_DSHOW is used to reduce the time taken to open the ext. camera
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    imgGry = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    thrash  = cv2.adaptiveThreshold(imgGry, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 8)
+    contours , _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    i = 0
+    
+    # list for storing names of shapes
+    for contour in contours:
+    
+        # here we are ignoring first counter because 
+        # findcontour function detects whole image as shape
+        if i == 0:
+            i = 1
+            continue
+    
+        # cv2.approxPloyDP() function to approximate the shape
+        approx = cv2.approxPolyDP(
+            contour, 0.01 * cv2.arcLength(contour, True), True)
+        
+
+        # finding center point of shape
+        M = cv2.moments(contour)
+        if M['m00'] != 0.0:
+            x = int(M['m10']/M['m00'])
+            y = int(M['m01']/M['m00'])
+
+        for aa in approx:
+            dist= math.sqrt((aa[0][0]-x)**2+(aa[0][1]-y)**2)
+            if dist >=35:
+                # using drawContours() function
+                cv2.drawContours(frame, [contour], 0, (0, 0, 255), 3)
+
+                # putting shape name at center of each shape
+                if len(approx) == 3:
+                    cv2.putText(frame, 'Triangle', (x, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+            
+                elif len(approx) == 4:
+                    cv2.putText(frame, 'Quadrilateral', (x, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        
+                elif len(approx) == 5:
+
+                    cv2.putText(frame, 'Pentagon', (x, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        
+                elif len(approx) == 6:
+                    cv2.putText(frame, 'Hexagon', (x, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        
+                else:
+                    if dist <= 120:
+                        cv2.putText(frame, 'ARM', (x, y),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+
+
+    cv2.imshow('shapes', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
