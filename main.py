d5ca046078fd3c9e2818b9bdb00d6df2517ce31d
Erik Brusewitz <eribrus@student.chalmers.se>
2022-10-05 13:33:49 +0200

    big structural changes

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     cap = vision.Initialize_camera()
     
     if (hubert == -1 or cap == -1):
-        raise Exception("Exiting program...)
+        raise Exception("Exiting program...")
     
     
     shapes = ["Quadrilateral", "Pentagon", "Hexagon"]
